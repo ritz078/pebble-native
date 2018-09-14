eb1c77571c1d17f4b638db42eaf643b00289d7ad
ritz078 <rkritesh078@gmail.com>
2018-09-14 13:30:29 +0530

    feat(select): add option to pass label component

--- src/components/typings/Select.ts
+++ src/components/typings/Select.ts
@@ -6,6 +6,12 @@ export interface SelectProps extends OptionsProps {
   errorMessage?: string;
   valueExtractor?: (item: any) => string;
   disabled?: boolean;
+  label?: (
+    args: {
+      value: string;
+      props: OptionsProps;
+    }
+  ) => JSX.Element;
 }
 
 export interface SelectState {
